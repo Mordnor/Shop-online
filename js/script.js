822bb0df96485d4d259cad8ace0cbf8acbe98ae4
Mordnor <dorian.pitiot@gmail.com>
2018-07-10 10:06:46 +0200

    panier charge toutes les pages

--- js/script.js
+++ js/script.js
@@ -6,3 +6,52 @@ $('.bxslider').bxSlider({
     speed: 500,
     mode: "horizontal",
 });
+
+$(document).ready(function() {
+
+    var GET_PARAM = function(name) {
+        return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+    };
+
+    var i = GET_PARAM("page_produit_id");
+    var myproduct = catalog[i];
+
+    var cart = {};
+    function saveCart() {
+        var cart_json = JSON.stringify(cart);
+        localStorage.setItem("product", cart_json);
+    }
+
+    function loadCart() {
+        var cart_json = localStorage.getItem("product");
+        cart = JSON.parse(cart_json)||{};
+    }
+
+    function displayCart() {
+        $("#cart ul").html("")
+        for (var i in cart) {
+            $("#cart ul").append($(`<li class="list-group-item">${catalog[i].name} x ${cart[i]}</li>`));
+        }
+    }
+
+    loadCart()
+    displayCart()
+
+    $("#ajout_panier").click(function() {
+        var productId = i
+
+        if (cart[productId]) {
+            cart[productId]++
+        } else {
+            cart[productId] = 1
+        }
+        saveCart()
+        displayCart()
+    })
+
+    function countTotal() {
+
+    }
+
+
+});
